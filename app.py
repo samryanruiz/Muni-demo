944910d10dec493ef1bc4d7f15881cfb1ed8492f
Christian Del Rosario <ainsley.drosario@gmail.com>
2023-04-25 20:15:47 +0800

    added pages prediction

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+@app.route('/classifier')
+def classifier():
+    return render_template("classifierPage.html")
+
+@app.route('/classifier/predict', methods=['POST'])
 async def predict():
     text = request.form.get('input_text')
     data = {"inputs": text}
@@ -20,11 +24,46 @@ async def predict():
     results = results[0][0]
     label = results['label']
     score = results['score']
-    rounded_score = round(score*100, 4)
+    rounded_score = round(score*100)
     results['score'] = rounded_score
     await asyncio.sleep(0.1)
-
+    print(label, score)
     return results
+
+@app.route('/anger')
+def anger():
+    score = request.args.get('score')
+    return render_template("anger.html", score=score)
+
+@app.route('/disgust')
+def disgust():
+    score = request.args.get('score')
+    return render_template("disgust.html", score=score)
+
+@app.route('/fear')
+def fear():
+    score = request.args.get('score')
+    return render_template("fear.html", score=score)
+
+@app.route('/joy')
+def joy():
+    score = request.args.get('score')
+    return render_template("joy.html", score=score)
+
+@app.route('/neutral')
+def neutral():
+    score = request.args.get('score')
+    return render_template("neutral.html", score=score)
+
+@app.route('/sadness')
+def sadness():
+    score = request.args.get('score')
+    return render_template("sadness.html", score=score)
+
+@app.route('/surprise')
+def surprise():
+    score = request.args.get('score')
+    return render_template("surprise.html", score=score)
 
 if __name__ == '__main__':
     app.run()
